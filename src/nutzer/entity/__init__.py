3dda106aca1c5d46561c11909871aba61c8c2ea8
hienfinity <hiendo2000@yahoo.de>
2026-03-31 20:33:16 +0200

    add interesse enum

--- src/nutzer/entity/__init__.py
+++ src/nutzer/entity/__init__.py
@@ -1,7 +1,8 @@
 """Entity-Paket fuer die Domaene Nutzer."""
 
 from nutzer.entity.base import Base
+from nutzer.entity.interesse import Interesse
 from nutzer.entity.rolle import Rolle
 from nutzer.entity.status import Status
 
-__all__ = ["Base", "Rolle", "Status"]
+__all__ = ["Base", "Interesse", "Rolle", "Status"]
