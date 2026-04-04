7031ededbbfc19e53d0f81a704958a787da1ca69
hienfinity <hiendo2000@yahoo.de>
2026-04-04 04:41:39 +0200

    add class NutzerRepo

---
+++ src/nutzer/repository/nutzer_repository.py
@@ -0,0 +1,7 @@
+"""Repository fuer persistente Nutzerdaten."""
+
+__all__ = ["NutzerRepository"]
+
+
+class NutzerRepository:
+    """Stellt spaeter DB-Zugriffe fuer die Entity-Klasse Nutzer bereit."""
