d02433a61adb210edf4793e4ffc8816095be7b93
Minh Nguyen <qminhnguyen99@gmail.com>
2026-04-04 17:56:00 +0200

    added imports

--- src/nutzer/repository/nutzer_repository.py
+++ src/nutzer/repository/nutzer_repository.py
@@ -1,7 +1,18 @@
+
 """Repository fuer persistente Nutzerdaten."""
 
-__all__ = ["NutzerRepository"]
+from collections.abc import Mapping, Sequence
+from typing import Final
 
+from loguru import logger
+from sqlalchemy import func, select
+from sqlalchemy.orm import Session, joinedload
+
+from nutzer.entity import Nutzer
+from nutzer.repository.pageable import Pageable
+from nutzer.repository.slice import Slice
+
+__all__ = ["NutzerRepository"]
 
 class NutzerRepository:
     """Stellt spaeter DB-Zugriffe fuer die Entity-Klasse Nutzer bereit."""
