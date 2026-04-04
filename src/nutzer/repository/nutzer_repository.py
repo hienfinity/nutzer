c42afa52366e6cdb95c32631980784eace2135ac
Minh Nguyen <qminhnguyen99@gmail.com>
2026-04-04 18:03:38 +0200

    created find by id

--- src/nutzer/repository/nutzer_repository.py
+++ src/nutzer/repository/nutzer_repository.py
@@ -15,4 +15,30 @@ from nutzer.repository.slice import Slice
 __all__ = ["NutzerRepository"]
 
 class NutzerRepository:
-    """Stellt spaeter DB-Zugriffe fuer die Entity-Klasse Nutzer bereit."""
+    """Repository-Klasse mit CRUD-Methoden für die Entity-Klasse Nutzer."""
+
+    def find_by_id(self, nutzer_id: int | None, session: Session) -> Nutzer | None:
+        """Suche mit der Nutzer-ID.
+
+        :param nutzer_id: ID des gesuchten Nutzers
+        :param session: Session für SQLAlchemy
+        :return: Der gefundene Nutzer oder None
+        :rtype: Nutzer | None
+        """
+        logger.debug("nutzer_id={}", nutzer_id)
+
+        if nutzer_id is None:
+            return None
+
+        statement: Final = (
+            select(Nutzer)
+            .options(
+                joinedload(Nutzer.adresse),
+                joinedload(Nutzer.einstellung),
+            )
+            .where(Nutzer.id == nutzer_id)
+        )
+        nutzer: Final = session.scalar(statement)
+
+        logger.debug("{}", nutzer)
+        return nutzer
