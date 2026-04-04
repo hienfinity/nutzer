4cde4bc40e96129a55d845d39fbfbb85c4a64e24
hienfinity <hiendo2000@yahoo.de>
2026-04-05 01:43:42 +0200

    add __all__ in __init__.py

--- src/nutzer/repository/__init__.py
+++ src/nutzer/repository/__init__.py
@@ -1,1 +1,15 @@
 """Paket fuer den Datenbankzugriff der Domaene Nutzer."""
+
+from nutzer.repository.nutzer_repository import NutzerRepository
+from nutzer.repository.pageable import MAX_PAGE_SIZE, Pageable
+from nutzer.repository.session_factory import Session, engine
+from nutzer.repository.slice import Slice
+
+__all__ = [
+    "MAX_PAGE_SIZE",
+    "NutzerRepository",
+    "Pageable",
+    "Session",
+    "Slice",
+    "engine",
+]
