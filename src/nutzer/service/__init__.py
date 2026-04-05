64e299100d02b4553f3960bfda7ed6a326f72cc0
hienfinity <hiendo2000@yahoo.de>
2026-04-05 03:18:34 +0200

    extend __init__

--- src/nutzer/service/__init__.py
+++ src/nutzer/service/__init__.py
@@ -1,6 +1,15 @@
-"""Paket fuer die Service-Schicht der Domaene Nutzer."""
+"""Modul fuer die Geschaeftslogik von Nutzer."""
 
+from nutzer.service.adresse_dto import AdresseDTO
+from nutzer.service.einstellung_dto import EinstellungDTO
 from nutzer.service.exceptions import NotFoundError
+from nutzer.service.nutzer_dto import NutzerDTO
 from nutzer.service.nutzer_service import NutzerService
 
-__all__ = ["NotFoundError", "NutzerService"]
+__all__ = [
+    "AdresseDTO",
+    "EinstellungDTO",
+    "NotFoundError",
+    "NutzerDTO",
+    "NutzerService",
+]
