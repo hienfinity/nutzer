28fdcc6ec550ae3386933e8ed51a30ba283d0c92
hienfinity <hiendo2000@yahoo.de>
2026-04-05 02:48:47 +0200

    add find

--- src/nutzer/service/__init__.py
+++ src/nutzer/service/__init__.py
@@ -1,5 +1,9 @@
-"""Paket fuer die Service-Schicht der Domaene Nutzer."""
+"""Modul fuer die Geschaeftslogik von Nutzer."""
 
+from nutzer.service.exceptions import NotFoundError
 from nutzer.service.nutzer_service import NutzerService
 
-__all__ = ["NutzerService"]
+# https://docs.python.org/3/tutorial/modules.html#importing-from-a-package
+# Weitere Exporte wie NutzerDTO oder NutzerWriteService kommen dazu,
+# sobald die zugehoerigen Dateien im Paket existieren.
+__all__ = ["NotFoundError", "NutzerService"]
--- src/nutzer/service/nutzer_service.py
+++ src/nutzer/service/nutzer_service.py
@@ -1,11 +1,13 @@
 """Geschaeftslogik zum Lesen von Nutzerdaten."""
 
+from collections.abc import Mapping
 from typing import Final
 
 from loguru import logger
 
 from nutzer.entity import Nutzer
-from nutzer.repository import NutzerRepository, Session
+from nutzer.repository import NutzerRepository, Pageable, Session, Slice
+from nutzer.service.exceptions import NotFoundError
 
 __all__ = ["NutzerService"]
 
@@ -17,13 +19,37 @@ class NutzerService:
         """Konstruktor mit abhaengigem NutzerRepository."""
         self.repo: NutzerRepository = repo
 
-    def find_by_id(self, nutzer_id: int) -> Nutzer | None:
+    def find_by_id(self, nutzer_id: int) -> Nutzer:
         """Einen Nutzer ueber seine ID suchen."""
         logger.debug("nutzer_id={}", nutzer_id)
 
         with Session() as session:
             nutzer: Final = self.repo.find_by_id(nutzer_id=nutzer_id, session=session)
+            if nutzer is None:
+                logger.debug("NotFoundError fuer nutzer_id={}", nutzer_id)
+                raise NotFoundError(nutzer_id=nutzer_id)
             session.commit()
 
         logger.debug("{}", nutzer)
         return nutzer
+
+    def find(
+        self,
+        suchparameter: Mapping[str, str],
+        pageable: Pageable,
+    ) -> Slice[Nutzer]:
+        """Nutzer mit Suchparametern suchen."""
+        logger.debug("{}", suchparameter)
+
+        with Session() as session:
+            nutzer_slice: Final = self.repo.find(
+                suchparameter=suchparameter,
+                pageable=pageable,
+                session=session,
+            )
+            if len(nutzer_slice.content) == 0:
+                raise NotFoundError()
+            session.commit()
+
+        logger.debug("{}", nutzer_slice)
+        return nutzer_slice
