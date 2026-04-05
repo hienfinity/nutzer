0309e3c874d77d4072291fcc332c3cf012a2a60b
hienfinity <hiendo2000@yahoo.de>
2026-04-05 02:58:42 +0200

    add einstellung_dto

--- src/nutzer/service/__init__.py
+++ src/nutzer/service/__init__.py
@@ -1,16 +1,6 @@
-"""Modul fuer die Geschaeftslogik von Nutzer."""
+"""Paket fuer die Service-Schicht der Domaene Nutzer."""
 
-from nutzer.service.adresse_dto import AdresseDTO
-from nutzer.service.einstellung_dto import EinstellungDTO
 from nutzer.service.exceptions import NotFoundError
-from nutzer.service.nutzer_dto import NutzerDTO
 from nutzer.service.nutzer_service import NutzerService
 
-# https://docs.python.org/3/tutorial/modules.html#importing-from-a-package
-__all__ = [
-    "AdresseDTO",
-    "EinstellungDTO",
-    "NotFoundError",
-    "NutzerDTO",
-    "NutzerService",
-]
+__all__ = ["NotFoundError", "NutzerService"]
