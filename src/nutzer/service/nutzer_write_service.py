20ae702a74d18f360d16fe3f88bf3d9b78260dd0
Minh Nguyen <qminhnguyen99@gmail.com>
2026-04-09 01:34:00 +0200

    implemented imports

---
+++ src/nutzer/service/nutzer_write_service.py
@@ -0,0 +1,17 @@
+"""Geschaeftslogik zum Schreiben von Nutzerdaten."""
+
+from typing import Final
+
+from loguru import logger
+
+from nutzer.entity import Nutzer
+from nutzer.repository import NutzerRepository, Session
+from nutzer.service.exceptions import (
+    EmailExistsError,
+    NotFoundError,
+    UsernameExistsError,
+    VersionOutdatedError,
+)
+from nutzer.service.nutzer_dto import NutzerDTO
+
+__all__ = ["NutzerWriteService"]
