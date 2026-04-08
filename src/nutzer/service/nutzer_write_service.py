41121e125daaad2a7ca39dcb4ca218afb149bbb6
Minh Nguyen <qminhnguyen99@gmail.com>
2026-04-09 01:48:44 +0200

    implemented create

--- src/nutzer/service/nutzer_write_service.py
+++ src/nutzer/service/nutzer_write_service.py
@@ -21,3 +21,35 @@ class NutzerWriteService:
     def __init__(self, repo: NutzerRepository) -> None:
         """Konstruktor mit abhaengigem NutzerRepository."""
         self.repo: NutzerRepository = repo
+
+    def create(self, nutzer: Nutzer) -> NutzerDTO:
+        """Einen neuen Nutzer anlegen.
+
+        :param nutzer: Der neue Nutzer ohne ID
+        :return: Der neu angelegte Nutzer mit generierter ID
+        :rtype: NutzerDTO
+        :raises EmailExistsError: Falls die Emailadresse bereits existiert
+        :raises UsernameExistsError: Falls der Benutzername bereits existiert
+        """
+        logger.debug(
+            "nutzer={}, adresse={}, einstellung={}",
+            nutzer,
+            nutzer.adresse,
+            nutzer.einstellung,
+        )
+
+        with Session() as session:
+            email: Final = nutzer.email
+            if self.repo.exists_email(email=email, session=session):
+                raise EmailExistsError(email=email)
+
+            username: Final = nutzer.username
+            if self.repo.exists_username(username=username, session=session):
+                raise UsernameExistsError(username)
+
+            nutzer_db: Final = self.repo.create(nutzer=nutzer, session=session)
+            nutzer_dto: Final = NutzerDTO(nutzer_db)
+            session.commit()
+
+        logger.debug("nutzer_dto={}", nutzer_dto)
+        return nutzer_dto
