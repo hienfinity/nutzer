af3d7bc7f5dddc500abe6596a96470b07918c406
Minh Nguyen <qminhnguyen99@gmail.com>
2026-04-09 02:08:59 +0200

    implemented class emailexistserror

--- src/nutzer/service/exceptions.py
+++ src/nutzer/service/exceptions.py
@@ -1,7 +1,11 @@
 """Exceptions in der Geschaeftslogik von Nutzer."""
 
-__all__ = ["NotFoundError"]
-
+__all__ = [
+    "NotFoundError",
+    "EmailExistsError",
+    "UsernameExistsError",
+    "VersionOutdatedError",
+]
 
 class NotFoundError(Exception):
     """Exception, falls kein Nutzer gefunden wurde."""
@@ -10,3 +14,11 @@ class NotFoundError(Exception):
         """Initialisierung mit der nicht gefundenen Nutzer-ID."""
         super().__init__("Not Found")
         self.nutzer_id = nutzer_id
+
+class EmailExistsError(Exception):
+    """Exception, falls die Emailadresse bereits existiert."""
+
+    def __init__(self, email: str) -> None:
+        """Initialisierung mit der existierenden Emailadresse."""
+        super().__init__("Email already exists")
+        self.email = email
