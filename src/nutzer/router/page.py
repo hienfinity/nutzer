7da42b0a0a02d549671f5ec833911aec2aeae651
Minh Nguyen <qminhnguyen99@gmail.com>
2026-04-10 01:56:45 +0200

    created class page

--- src/nutzer/router/page.py
+++ src/nutzer/router/page.py
@@ -11,3 +11,12 @@ __all__ = ["Page"]
 T = TypeVar("T")
 
 
+@dataclass(slots=True, kw_only=True)
+class Page(Generic[T]):
+    """Page fuer paginierte Ergebnisse."""
+
+    content: tuple[T, ...]
+    number: int
+    size: int
+    total_elements: int
+    total_pages: int
