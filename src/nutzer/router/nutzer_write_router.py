bf4bf8be036ffbdb3722e700013be5bb7d249ca5
Minh Nguyen <qminhnguyen99@gmail.com>
2026-04-11 00:47:58 +0200

    implemented put

--- src/nutzer/router/nutzer_write_router.py
+++ src/nutzer/router/nutzer_write_router.py
@@ -44,3 +44,68 @@ def post(
         status_code=status.HTTP_201_CREATED,
         headers={"Location": f"{request.url}/{nutzer_dto.id}"},
     )
+
+@nutzer_write_router.put(
+    "/{nutzer_id}",
+    # dependencies=[Depends(RolesRequired([Role.ADMIN, Role.NUTZER]))],
+)
+def put(
+    nutzer_id: int,
+    nutzer_update_model: NutzerUpdateModel,
+    request: Request,
+    service: Annotated[NutzerWriteService, Depends(get_write_service)],
+) -> Response:
+    """PUT-Request, um einen Nutzer zu aktualisieren.
+
+    :param nutzer_id: ID des zu aktualisierenden Nutzers als Pfadparameter
+    :param request: Injiziertes Request-Objekt mit If-Match im Header
+    :param service: Injizierter Service fuer Geschaeftslogik
+    :return: Response mit Statuscode 204
+    :rtype: Response
+    :raises EmailExistsError: Falls die neue Emailadresse bereits existiert
+    :raises UsernameExistsError: Falls der neue Benutzername bereits existiert
+    :raises NotFoundError: Falls zur ID kein Nutzer existiert
+    :raises VersionOutdatedError: Falls die Versionsnummer nicht aktuell ist
+    """
+    if_match_value: Final = request.headers.get(IF_MATCH)
+    logger.debug(
+        "nutzer_id={}, if_match={}, nutzer_update_model={}",
+        nutzer_id,
+        if_match_value,
+        nutzer_update_model,
+    )
+
+    if if_match_value is None:
+        return create_problem_details(
+            status_code=status.HTTP_428_PRECONDITION_REQUIRED,
+        )
+
+    if (
+        len(if_match_value) < IF_MATCH_MIN_LEN
+        or not if_match_value.startswith('"')
+        or not if_match_value.endswith('"')
+    ):
+        return create_problem_details(
+            status_code=status.HTTP_412_PRECONDITION_FAILED,
+        )
+
+    version: Final = if_match_value[1:-1]
+    try:
+        version_int: Final = int(version)
+    except ValueError:
+        return Response(
+            status_code=status.HTTP_412_PRECONDITION_FAILED,
+        )
+
+    nutzer: Final = nutzer_update_model.to_nutzer()
+    nutzer_modified: Final = service.update(
+        nutzer=nutzer,
+        nutzer_id=nutzer_id,
+        version=version_int,
+    )
+    logger.debug("nutzer_modified={}", nutzer_modified)
+
+    return Response(
+        status_code=status.HTTP_204_NO_CONTENT,
+        headers={ETAG: f'"{nutzer_modified.version}"'},
+    )
