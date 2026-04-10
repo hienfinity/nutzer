77b4b2d1d2723d5b946418b1916065a973400674
Minh Nguyen <qminhnguyen99@gmail.com>
2026-04-10 02:03:51 +0200

    implemented create

--- src/nutzer/router/page.py
+++ src/nutzer/router/page.py
@@ -20,3 +20,21 @@ class Page(Generic[T]):
     size: int
     total_elements: int
     total_pages: int
+
+    @staticmethod
+    def create(
+        content: tuple[T, ...],
+        pageable: Pageable,
+        total_elements: int,
+    ) -> "Page[T]":
+        """Erzeuge eine Page auf Basis von Content, Pageable und Gesamtanzahl."""
+        size = pageable.size
+        total_pages = ceil(total_elements / size) if size > 0 else 1
+
+        return Page(
+            content=content,
+            number=pageable.number,
+            size=size,
+            total_elements=total_elements,
+            total_pages=total_pages,
+        )
