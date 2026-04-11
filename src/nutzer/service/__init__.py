7dbfab48779a0ee9cc686f736e6ac46c6c338bc1
hienfinity <hiendo2000@yahoo.de>
2026-04-11 07:47:51 +0200

    export write service & exeptions in init

--- src/nutzer/service/__init__.py
+++ src/nutzer/service/__init__.py
@@ -2,14 +2,24 @@
 
 from nutzer.service.adresse_dto import AdresseDTO
 from nutzer.service.einstellung_dto import EinstellungDTO
-from nutzer.service.exceptions import NotFoundError
+from nutzer.service.exceptions import (
+    EmailExistsError,
+    NotFoundError,
+    UsernameExistsError,
+    VersionOutdatedError,
+)
 from nutzer.service.nutzer_dto import NutzerDTO
 from nutzer.service.nutzer_service import NutzerService
+from nutzer.service.nutzer_write_service import NutzerWriteService
 
 __all__ = [
     "AdresseDTO",
+    "EmailExistsError",
     "EinstellungDTO",
     "NotFoundError",
     "NutzerDTO",
     "NutzerService",
+    "NutzerWriteService",
+    "UsernameExistsError",
+    "VersionOutdatedError",
 ]
