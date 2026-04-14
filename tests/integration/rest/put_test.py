33662af5d61a3b96bf731c167bfde8086c661d4e
hienfinity <hiendo2000@yahoo.de>
2026-04-14 23:19:47 +0200

    add test_put_ungueltige_versionsnr

--- tests/integration/rest/put_test.py
+++ tests/integration/rest/put_test.py
@@ -195,4 +195,35 @@ def test_put_alte_versionsnr() -> None:
     assert response.status_code == HTTPStatus.PRECONDITION_FAILED
 
 
+@mark.rest
+@mark.put_request
+def test_put_ungueltige_versionsnr() -> None:
+    # arrange
+    nutzer_id: Final = 40
+    geaenderter_nutzer: Final = {
+        "vorname": "Alicia",
+        "nachname": "Testerput",
+        "email": EMAIL_UPDATE,
+        "telefonnummer": "654321",
+        "geburtsdatum": "2022-01-03",
+        "beitrittsdatum": "2024-01-03",
+        "aktiv": False,
+        "rolle": "NUTZER",
+        "status": "AKTIV",
+    }
+    headers = {"If-Match": '"xy"'}
+
+    # act
+    response: Final = put(
+        f"{rest_url}/{nutzer_id}",
+        json=geaenderter_nutzer,
+        headers=headers,
+        verify=ctx,
+    )
+
+    # assert
+    assert response.status_code == HTTPStatus.PRECONDITION_FAILED
+    assert not response.text
+
+
 
