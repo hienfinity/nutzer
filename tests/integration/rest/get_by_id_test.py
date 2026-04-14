d18d7982181b3b2e8f7a94c87e9d2812568b0faf
hienfinity <hiendo2000@yahoo.de>
2026-04-14 22:43:48 +0200

    add get by id test

---
+++ tests/integration/rest/get_by_id_test.py
@@ -0,0 +1,80 @@
+"""Tests fuer GET mit Pfadparameter fuer die ID."""
+
+from http import HTTPStatus
+from typing import Final
+
+from common_test import ctx, get, rest_url
+from pytest import mark
+
+
+@mark.rest
+@mark.get_request
+def test_get_by_id() -> None:
+    # arrange
+    nutzer_id: Final = 20
+
+    # act
+    response: Final = get(f"{rest_url}/{nutzer_id}", verify=ctx)
+
+    # assert
+    assert response.status_code == HTTPStatus.OK
+    response_body: Final = response.json()
+    assert isinstance(response_body, dict)
+    id_actual: Final = response_body.get("id")
+    assert id_actual is not None
+    assert id_actual == nutzer_id
+
+
+@mark.rest
+@mark.get_request
+def test_get_by_id_not_found() -> None:
+    # arrange
+    nutzer_id: Final = 999999
+
+    # act
+    response: Final = get(f"{rest_url}/{nutzer_id}", verify=ctx)
+
+    # assert
+    assert response.status_code == HTTPStatus.NOT_FOUND
+
+
+@mark.rest
+@mark.get_request
+def test_get_by_id_etag() -> None:
+    # arrange
+    nutzer_id: Final = 20
+    headers = {"If-None-Match": '"1"'}
+
+    # act
+    response: Final = get(
+        f"{rest_url}/{nutzer_id}",
+        headers=headers,
+        verify=ctx,
+    )
+
+    # assert
+    assert response.status_code == HTTPStatus.NOT_MODIFIED
+    assert not response.text
+
+
+@mark.rest
+@mark.get_request
+def test_get_by_id_etag_invalid() -> None:
+    # arrange
+    nutzer_id: Final = 20
+    headers = {"If-None-Match": "xxx"}
+
+    # act
+    response: Final = get(
+        f"{rest_url}/{nutzer_id}",
+        headers=headers,
+        verify=ctx,
+    )
+
+    # assert
+    assert response.status_code == HTTPStatus.OK
+    response_body: Final = response.json()
+    assert isinstance(response_body, dict)
+    id_actual: Final = response_body.get("id")
+    assert id_actual is not None
+    assert id_actual == nutzer_id
