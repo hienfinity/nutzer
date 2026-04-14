f408f8510dd73bf5013a2a1f9948c2d81450634f
hienfinity <hiendo2000@yahoo.de>
2026-04-14 23:18:40 +0200

    add test_put_nicht_vorhanden()

--- tests/integration/rest/put_test.py
+++ tests/integration/rest/put_test.py
@@ -75,4 +75,34 @@ def test_put_invalid() -> None:
     assert "rolle" in response.text
 
 
+@mark.rest
+@mark.put_request
+def test_put_nicht_vorhanden() -> None:
+    # arrange
+    nutzer_id: Final = 999999
+    geaenderter_nutzer: Final = {
+        "vorname": "Alicia",
+        "nachname": "Testerput",
+        "email": EMAIL_UPDATE,
+        "telefonnummer": "654321",
+        "geburtsdatum": "2022-01-03",
+        "beitrittsdatum": "2024-01-03",
+        "aktiv": False,
+        "rolle": "NUTZER",
+        "status": "AKTIV",
+    }
+    headers = {"If-Match": '"0"'}
+
+    # act
+    response: Final = put(
+        f"{rest_url}/{nutzer_id}",
+        json=geaenderter_nutzer,
+        headers=headers,
+        verify=ctx,
+    )
+
+    # assert
+    assert response.status_code == HTTPStatus.NOT_FOUND
+
+
 
