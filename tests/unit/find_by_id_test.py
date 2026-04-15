d1be75795993dba118cfedabc933baf56bfed92e
Minh Nguyen <qminhnguyen99@gmail.com>
2026-04-15 18:53:32 +0200

    implemented test_find:_by_id

--- tests/unit/find_by_id_test.py
+++ tests/unit/find_by_id_test.py
@@ -25,3 +25,54 @@ def session_mock(mocker: "MockerFixture"):
         ),
     )
     return session
+
+
+@mark.unit
+@mark.unit_find_by_id
+def test_find_by_id(nutzer_service, session_mock) -> None:
+    # Arrange
+    nutzer_id = 1
+    adresse_mock = Adresse(
+        id=11,
+        strasse="Teststrasse",
+        hausnummer="10",
+        plz="11111",
+        ort="Mockort",
+        nutzer_id=nutzer_id,
+        nutzer=None,
+    )
+    einstellung_mock = Einstellung(
+        id=21,
+        newsletter_aktiv=True,
+        benachrichtigungen_aktiv=True,
+        sprache="de",
+        nutzer_id=nutzer_id,
+        nutzer=None,
+    )
+    nutzer_mock = Nutzer(
+        id=nutzer_id,
+        vorname="Max",
+        nachname="Mocktest",
+        email="mock@email.test",
+        username="mocktest",
+        telefonnummer="123456789",
+        geburtsdatum=date(2000, 1, 1),
+        beitrittsdatum=date(2025, 1, 31),
+        aktiv=True,
+        rolle=Rolle.NUTZER,
+        status=Status.AKTIV,
+        adresse=adresse_mock,
+        einstellung=einstellung_mock,
+        interessen=[Interesse.TECHNIK],
+    )
+    adresse_mock.nutzer = nutzer_mock
+    einstellung_mock.nutzer = nutzer_mock
+
+    nutzer_dto_mock = NutzerDTO(nutzer_mock)
+    session_mock.commit.return_value = None
+
+    # Act
+    nutzer_dto = nutzer_service.find_by_id(nutzer_id=nutzer_id)
+
+    # Assert
+    assert asdict(nutzer_dto) == asdict(nutzer_dto_mock)
