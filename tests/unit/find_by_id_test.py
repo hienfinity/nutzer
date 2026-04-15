84f309b5c0f0b8b420cb9e354aaa1b5075050cb2
Minh Nguyen <qminhnguyen99@gmail.com>
2026-04-15 18:49:00 +0200

    implemented fixture session_mocks

--- tests/unit/find_by_id_test.py
+++ tests/unit/find_by_id_test.py
@@ -11,3 +11,17 @@ from nutzer.service import NotFoundError, NutzerDTO, NutzerService
 
 if TYPE_CHECKING:
     from pytest_mock import MockerFixture
+
+
+@fixture
+def session_mock(mocker: "MockerFixture"):
+    session = mocker.Mock()
+    # Patching von "with Session() as session:" in nutzer_service.py
+    mocker.patch(
+        "nutzer.service.nutzer_service.Session",
+        return_value=mocker.MagicMock(
+            __enter__=lambda self: session,
+            __exit__=lambda self, exc_type, exc, tb: None,
+        ),
+    )
+    return session
